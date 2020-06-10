61c9f7a7954182090f07e2166d9034fdf6a367ff
Evan hameed <evanhameed9966@gmail.com>
2020-06-10 14:42:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ function map(sourceArray,work){
     return newArr;
 }
 function reduce(arr, callback, startingValue){
+<<<<<<< HEAD
   let memo = (!!startingValue) ? startingValue : arr[0]
   // let i = (!!starting) ? 0 : 1
 
   for (let i= (!!startingValue) ? 0 : 1 ; i < arr.length; i++) {
+=======
+  let memo = (!!startingValue) ? starting : arr[0]
+  // let i = (!!starting) ? 0 : 1
+
+  for (let i= (!!starting) ? 0 : 1 ; i < src.length; i++) {
+>>>>>>> 5543aaf8790d76e814c42412a475cf89a61ff08a
     memo = callback(arr[i], memo)
   }
 
   return memo;
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 5543aaf8790d76e814c42412a475cf89a61ff08a
